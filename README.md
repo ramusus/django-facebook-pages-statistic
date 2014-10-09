5763d7f14ecf0109bb3e94269d0c230ea606f8b5
krupin.dv <krupin.dv19@gmail.com>
2014-10-09 18:37:16 +0400

    setup, license, etc...

---
+++ README.md
@@ -0,0 +1,35 @@
+# Django Facebook Pages Statistic
+
+Extends https://github.com/ramusus/django-facebook-pages
+add likes and shares parse for FB pages
+
+
+## Installation
+
+    pip install facebook-pages-statistic
+
+Add into `settings.py` lines:
+
+    INSTALLED_APPS = (
+        ...
+        'taggit',
+        'oauth_tokens',
+        'facebook_api',
+        'facebook_pages',
+        'facebook-pages-statistic',
+    )
+
+
+## Usage examples
+    First create page
+    see https://github.com/ramusus/django-facebook-pages
+
+    supose what we already have page with name 'cocacola'
+
+    >>> from fb_parse.models import Page
+    >>> p = Page.objects.get_with_updated_stats(name='cocacola')
+    >>> p.last_stats.likes
+    89131588
+    >>> p.last_stats.shares
+    927945
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-facebook-pages-statistic',
+    version=__import__('facebook-pages-statistic').__version__,
+    description='Extends Django django-facebook-pages app with statistic',
+    long_description=open('README.md').read(),
+    author='krupin.dv',
+    author_email='krupin.dv19@gmail.com',
+    url='https://github.com/Andertaker/django-facebook-pages-statistic',
+    download_url='http://pypi.python.org/pypi/django-facebook-pages-statistic',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False, # because we're including media that Django needs
+    install_requires=[
+        'django-facebook-pages',
+        'requests>=2.4.3'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
