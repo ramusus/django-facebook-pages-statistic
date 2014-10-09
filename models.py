38dda30d98ed719e202c2f409c62d4e241f1eba8
krupin.dv <krupin.dv19@gmail.com>
2014-10-09 18:08:48 +0400

    extend django-facebook-pages

--- models.py
+++ models.py
@@ -2,36 +2,71 @@ from django.db import models
 from django.utils import timezone
 
 from fbparse import likes_shares_parse
+from facebook_pages.models import Page
 
 
-class PageStatisticManager(models.Manager):
 
-    def get_with_update(self, *args, **kwargs):
-        fbpage = self.get(*args, **kwargs)
-        fbpage.save() # calls likes & shares update
-        return fbpage
+
+class PageManager(models.Manager):
+
+    def get_with_updated_stats(self, *args, **kwargs):
+        page = self.get(*args, **kwargs)
+        page.update_stats()
+        return page
+
+
+
+
+class Page(Page):
+
+    objects = PageManager()
+
+    def update_stats(self):
+        self.pagestatistic_set.create()
+        return self
+
+    @property
+    def last_stats(self):
+        return self.pagestatistic_set.latest()
+
+
+    class Meta(Page.Meta):
+        proxy = True
+        #app_label = 'facebook_pages'
+
+
+
+#class PageStatisticManager(models.Manager):
+#
+#    def get_with_update(self, *args, **kwargs):
+#        fbpage = self.get(*args, **kwargs)
+#        fbpage.save() # calls likes & shares update
+#        return fbpage
 
 
 class PageStatistic(models.Model):
-    page = models.CharField("Page", max_length=64)
+    #page = models.CharField("Page", max_length=64)
+    page = models.ForeignKey(Page)
+
     likes = models.PositiveIntegerField(default=0, editable=False)
     shares = models.PositiveIntegerField(default=0, editable=False)
-    last_update = models.DateTimeField("Last update time", editable=False) # default=timezone.now()
+    last_update = models.DateTimeField("Last update time", editable=False, auto_now_add=True)
 
-    objects = PageStatisticManager()
+    #objects = PageStatisticManager()
 
-    def __unicode__(self):
-        return self.page
+#    def __unicode__(self):
+#        return self.page
 
     class Meta:
         verbose_name = "PageStatistic"
         verbose_name_plural = "PageStatistic"
+        get_latest_by = 'last_update'
 
     def save(self, *args, **kwargs):
-        d = likes_shares_parse(self.page)
+        page = self.page.name
+        d = likes_shares_parse(page)
         self.likes = d["likes"]
         self.shares = d["shares"]
-        self.last_update = timezone.now()
 
         return super(self.__class__, self).save(*args, **kwargs)
 
