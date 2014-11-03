260df20ebda259798215d6e4343c5e7aadc55a10
ramusus <ramusus@gmail.com>
2014-11-03 21:53:27 +0300

    refactored tests, add extras

--- facebook_pages_statistic/tests.py
+++ facebook_pages_statistic/tests.py
@@ -2,43 +2,38 @@ from django.utils import timezone
 from django.test import TestCase
 from facebook_pages.models import Page
 from facebook_pages.factories import PageFactory
-
 from models import PageStatistic
+from datetime import datetime
+
+PAGE_FANS_ID = 19292868552
 
 
-PAGE_FANS_ID = 501842786534856
-
-
-class PageStatisticTest(TestCase):
-
-    def setUp(self):
-        self.t = timezone.now()
-        self.p = PageFactory(graph_id=PAGE_FANS_ID, likes_count=10, talking_about_count=20)
+class FacebookPageStatisticTest(TestCase):
 
     def test_page_statistic_create(self):
-        t = self.t
-        p = self.p
 
-        ps = PageStatistic.objects.get(id=1)
-        self.assertEqual(ps.page, p)
-        self.assertEqual(ps.likes_count, p.likes_count)
-        self.assertEqual(ps.talking_about_count, p.talking_about_count)
-        self.assertGreater(ps.updated_at, t)
+        self.assertEqual(PageStatistic.objects.count(), 0)
 
-    def test_test_page_statistic_update(self):
-        t = timezone.now()
-        p = self.p
-        p.likes_count += 100
-        p.talking_about_count += 100
-        p.save()
+        page = PageFactory(graph_id=PAGE_FANS_ID, likes_count=10, talking_about_count=20)
 
-        ps = PageStatistic.objects.get(id=2)
-        self.assertEqual(ps.page, p)
-        self.assertEqual(ps.likes_count, p.likes_count)
-        self.assertEqual(ps.talking_about_count, p.talking_about_count)
-        self.assertGreater(ps.updated_at, t)
+        self.assertEqual(PageStatistic.objects.count(), 1)
+
+        stat = page.statistics.latest()
+        self.assertEqual(stat.likes_count, 10)
+        self.assertEqual(stat.talking_about_count, 20)
+        self.assertTrue(isinstance(stat.updated_at, datetime))
+
+        page = Page.remote.fetch(PAGE_FANS_ID)
+
+        self.assertEqual(PageStatistic.objects.count(), 2)
+
+        stat = page.statistics.latest()
+        self.assertTrue(stat.likes_count > 10)
+        self.assertTrue(stat.talking_about_count > 20)
+        self.assertTrue(isinstance(stat.updated_at, datetime))
 
     def test_null_stats_test(self):
-        count = PageStatistic.objects.count()
-        p = PageFactory(graph_id=123456, likes_count=None, talking_about_count=None)
-        self.assertEqual(PageStatistic.objects.count(), count)
+
+        p = PageFactory(likes_count=None, talking_about_count=None)
+
+        self.assertEqual(PageStatistic.objects.count(), 0)
